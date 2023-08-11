48b57be330006f3af2a1621195d92d675004acca
qinr <ruiqin_cs@foxmail.com>
2023-08-11 11:09:28 +0800

    feat(/users/signup) : add password encryption

--- internal/service/user.go
+++ internal/service/user.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/rui-cs/webook/internal/domain"
 	"github.com/rui-cs/webook/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var ErrUserDuplicateEmail = repository.ErrUserDuplicateEmail
@@ -18,5 +19,12 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 }
 
 func (svc *UserService) Signup(ctx context.Context, u domain.User) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hash)
+
 	return svc.repo.Create(ctx, u)
 }
